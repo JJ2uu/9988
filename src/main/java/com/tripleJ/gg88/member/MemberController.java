99712163f480a980891330f45bd273be8f72bd51
JJuu <jyj120572@gmail.com>
2023-08-13 04:40:51 +0900

    Feat : 약관동의 views 호출 메서드 추가

---
+++ src/main/java/com/tripleJ/gg88/member/MemberController.java
@@ -0,0 +1,14 @@
+package com.tripleJ.gg88.member;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MemberController {
+
+	@RequestMapping("member/account/agreement")
+	public String agreement() {
+		return "account/agreement";
+	}
+	
+}
