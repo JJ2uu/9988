04f17c75e797838eda801fc850c92121e533e61f
JJuu <jyj120572@gmail.com>
2023-08-22 17:26:08 +0900

    Feat : 컨트롤러, 서비스 분리 및 회원가입 로직 추가

--- src/main/java/com/tripleJ/gg88/controller/MemberController.java
+++ src/main/java/com/tripleJ/gg88/controller/MemberController.java
@@ -1,40 +1,49 @@
 package com.tripleJ.gg88.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.tripleJ.gg88.domain.MemberVO;
+import com.tripleJ.gg88.service.MemberService;
 
 @Controller
 @RequestMapping("member/*")
 public class MemberController {
+	
+	@Autowired
+	MemberService memberService;
 
 	@RequestMapping("account/agreement")
 	public String agreement() {
-		return "account/agreement";
+		return memberService.agreement();
 	}
 	
 	@RequestMapping("account/enter")
 	public String enter() {
-		return "account/enter";
+		return memberService.enter();
 	}
 	
 	@RequestMapping("account/completed")
-	public String completed() {
-		return "account/completed";
+	public String completed(String nickname, Model model) {
+		return memberService.completed(nickname, model);
 	}
 	
 	@RequestMapping("account/foundId")
 	public String foundId() {
-		return "account/foundId";
+		return memberService.foundId();
 	}
 	
 	@RequestMapping("account/notFoundId")
 	public String notFoundId() {
-		return "account/notFoundId";
+		return memberService.notFoundId();
 	}
 	
 	@RequestMapping("account/resetPw")
 	public String resetPw() {
-		return "account/resetPw";
+		return memberService.resetPw();
 	}
 	
 	@RequestMapping("account/changePw")
@@ -42,14 +51,32 @@ public class MemberController {
 		return "account/changePw";
 	}
 	
+	@RequestMapping("account/searchId")
+	@ResponseBody
+	public String searchId(String userId) {
+		return memberService.searchId(userId);
+	}
+	
+	@RequestMapping("account/searchNick")
+	@ResponseBody
+	public String searchNick(String nickname) {
+		return memberService.searchNick(nickname);
+	}
+	
+	@RequestMapping("account/signUp")
+	@ResponseBody
+	public int signUp(MemberVO memberVO, String userbirth) {
+		return memberService.signUp(memberVO, userbirth);
+	}
+	
 	@RequestMapping("info/myInfo")
 	public String myInfo() {
-		return "info/myInfo";
+		return memberService.myInfo();
 	}
 	
 	@RequestMapping("info/myHistory")
 	public String myHistory() {
-		return "info/myHistory";
+		return memberService.myHistory();
 	}
 	
 }
