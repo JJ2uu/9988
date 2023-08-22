9ebe37d9fddd01bacb6c1397618d568f85243fa8
JJuu <jyj120572@gmail.com>
2023-08-22 17:29:38 +0900

    Feat : MemberDAO 회원가입 로직

---
+++ src/main/java/com/tripleJ/gg88/repository/MemberDAO.java
@@ -0,0 +1,29 @@
+package com.tripleJ.gg88.repository;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.tripleJ.gg88.domain.MemberVO;
+
+@Repository
+public class MemberDAO {
+	
+	@Autowired
+	SqlSessionTemplate my;
+
+	public MemberVO searchId(String id) {
+		MemberVO result = my.selectOne("member.searchId", id);
+		return result;
+	}
+	
+	public MemberVO searchNick(String nickname) {
+		MemberVO result = my.selectOne("member.searchNick", nickname);
+		return result;
+	}
+	
+	public int signUp(MemberVO memberVO) {
+		int result = my.insert("member.signUp", memberVO);
+		return result;
+	}
+}
