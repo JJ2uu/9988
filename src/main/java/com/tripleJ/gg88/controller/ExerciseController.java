980a00bac44fb056578214b37b7092379e6c297e
정서현 <tjgus027@kakao.com>
2023-09-05 16:22:58 +0900

    Feat: 운동해요 유튜브 크롤링 테스트

--- src/main/java/com/tripleJ/gg88/controller/ExerciseController.java
+++ src/main/java/com/tripleJ/gg88/controller/ExerciseController.java
@@ -1,14 +1,28 @@
 package com.tripleJ.gg88.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.tripleJ.gg88.service.ExerciseService;
+
 @Controller
 @RequestMapping("exercise/*")
 public class ExerciseController {
-	@RequestMapping(value = "/doExercise", method = RequestMethod.GET)
-	public void mainPath(){
-		
+	private ExerciseService service;
+	
+	@Autowired
+	public ExerciseController(ExerciseService service) {
+		super();
+		this.service = service;
 	}
+
+	@RequestMapping(value = "/doExercise", method = RequestMethod.GET)
+	public void mainPath() throws Exception{
+		String test = service.search("운동");
+//		System.out.println(test);
+	}
+	
+	
 }
