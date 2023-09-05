6590f6d952e469d0f3f5a3f47eef074eaf70e9b3
정서현 <tjgus027@kakao.com>
2023-09-05 16:22:29 +0900

    Feat: 응급이에요 게시판 답글 초기 코드 작성

---
+++ src/main/java/com/tripleJ/gg88/repository/EmergencyReplyRepository.java
@@ -0,0 +1,29 @@
+package com.tripleJ.gg88.repository;
+
+import java.util.List;
+
+import com.tripleJ.gg88.domain.Emergency;
+import com.tripleJ.gg88.domain.EmergencyReply;
+
+public interface EmergencyReplyRepository {
+
+	void saveReply(EmergencyReply reply);
+
+	void findAllReply();
+
+	void updateReply(EmergencyReply reply);
+
+	void deleteReply(EmergencyReply reply);
+
+	void searchCommend(EmergencyReply reply);
+
+	//추천수 관련 메서드 4개
+	void createCommendCount();
+
+	int searchCommend(int like);
+
+	void deleteCommend(int like);
+
+	List<Emergency> listLike(String memberNo);
+
+}
---
+++ src/main/java/com/tripleJ/gg88/repository/EmergencyReplyRepositoryImpl.java
@@ -0,0 +1,68 @@
+package com.tripleJ.gg88.repository;
+
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.tripleJ.gg88.domain.Emergency;
+import com.tripleJ.gg88.domain.EmergencyReply;
+
+@Repository
+public class EmergencyReplyRepositoryImpl implements EmergencyReplyRepository {
+	String namespace = "emergencyReply.";
+	public final SqlSessionTemplate sql;
+	public EmergencyReplyRepositoryImpl(SqlSessionTemplate sql) {
+		super();
+		this.sql = sql;
+	}
+	
+	@Override
+	public void saveReply(EmergencyReply reply) {
+		sql.insert(namespace + "saveReply" + reply);
+	}
+	
+	@Override
+	public void findAllReply() {
+		sql.selectList(namespace + "findAllReply");
+	}
+	
+	@Override
+	public void updateReply(EmergencyReply reply) {
+		sql.update(namespace + "updateReply" + reply);
+	}
+	
+	@Override
+	public void deleteReply(EmergencyReply reply) {
+		int emergencyId = reply.getEmergencyId();
+		sql.delete(namespace + "deleteReply" + emergencyId);
+	}
+	
+	@Override
+	public void searchCommend(EmergencyReply reply) {
+		int commendCount = reply.getCommend_count();
+		sql.selectList(namespace + "searchCommend" + commendCount);
+	}
+	
+	//추천수 관련 메서드 4개
+	@Override
+	public void createCommendCount() {
+		
+	}
+	
+	@Override
+	public int searchCommend(int like) {
+		return 0;
+	}
+	
+	@Override
+	public void deleteCommend(int like) {
+		
+	}
+	
+	@Override
+	public List<Emergency> listLike(String memberNo) {
+		return sql.selectList(memberNo);
+	}
+	
+}
