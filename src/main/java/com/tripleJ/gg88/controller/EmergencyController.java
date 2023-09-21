13bc5e8603a0a015ae2327a3e03cb0937d180968
정서현 <tjgus027@kakao.com>
2023-09-21 16:41:12 +0900

    feat: 응급이에요 컨트롤러 구현

--- src/main/java/com/tripleJ/gg88/controller/EmergencyController.java
+++ src/main/java/com/tripleJ/gg88/controller/EmergencyController.java
@@ -1,14 +1,21 @@
 package com.tripleJ.gg88.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.tripleJ.gg88.service.EmergencyService;
 import com.tripleJ.gg88.domain.Emergency;
+import com.tripleJ.gg88.model.PagingDto;
 
 @Controller
 @RequestMapping("emergency/*")
@@ -21,28 +28,41 @@ public class EmergencyController {
 	}
 
 	@RequestMapping(value = "/main", method = RequestMethod.GET)
-	public void emergencyPath() {
-		service.findAllBoard();
-	}
-
-	@RequestMapping(value = "/createBoard", method = RequestMethod.GET)
-	public void createBoard() {
-		
+	public void emergencyPath(@RequestParam(defaultValue = "1") int currentNum, Model model) {
+		PagingDto page = service.paganation(currentNum);
+		List<Emergency> emergency = service.getList(page);
+		model.addAttribute("emergencyList", emergency);
 	}
 	
-	@RequestMapping(value = "/saveBoard", method = RequestMethod.GET)
+	@RequestMapping(value = "/createBoard", method = RequestMethod.GET)
+	public void createBoard() {
+	}
+	
+	@RequestMapping(value = "/saveBoard", method = RequestMethod.POST)
 	public String saveBoard(Emergency emergency, HttpServletRequest request) {
-		service.saveBoard(emergency, request);
+		HttpSession session = request.getSession();
+		String memberId = String.valueOf(session.getAttribute(""));
+		service.saveBoard(emergency, 1);
 		return "redirect:/emergency/main";
 	}
 	
 	@RequestMapping(value = "/temporarySave", method = RequestMethod.GET)
-	public void temporarySave(Emergency emergency) {
-		service.temporarySave(emergency);
+	public void temporarySave(Emergency emergency, HttpServletRequest request) {
+		HttpSession session = request.getSession();
+		String memberId = String.valueOf(session.getAttribute(""));
+		service.temporarySave(emergency, 1);
 	}
 	
 	@RequestMapping(value = "/board", method = RequestMethod.GET)
-	public void separateBoard(Emergency emergency) {
-		service.findById(emergency.getEmergencyId());
+	public String getBoard(int emergencyId, Model model) {
+		model.addAttribute("emergency", service.findById(emergencyId));
+		return "emergency/board";
 	}
+	
+	@RequestMapping(value = "/getPage", method = RequestMethod.POST)
+	@ResponseBody
+	public PagingDto getPage(@RequestParam(defaultValue = "1") int currentNum, Model model) {
+		return service.paganation(currentNum);
+	}
+	
 }
