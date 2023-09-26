baab49c710c8a0f8dd0945596f83aa3e708c32bc
정서현 <tjgus027@kakao.com>
2023-09-26 14:56:37 +0900

    style: 임시 저장 메서드 삭제

--- src/main/java/com/tripleJ/gg88/repository/EmergencyRepository.java
+++ src/main/java/com/tripleJ/gg88/repository/EmergencyRepository.java
@@ -8,8 +8,6 @@ import com.tripleJ.gg88.model.PagingDto;
 public interface EmergencyRepository {
 	int save(Emergency emergency); //저장
 	
-	int temporarySave(Emergency emergency); //임시 저장
-	
 	Emergency findById(int emergencyId); //아이디로 게시판 검색
 	
 	List<Emergency> findAll(); //전체 리스트
@@ -23,4 +21,6 @@ public interface EmergencyRepository {
 	int getTotalCount();
 	
 	List<Emergency> getList(PagingDto pagingDto);
+	
+	public int getMemberNo(String userId); //멤버 번호 찾기
 }
